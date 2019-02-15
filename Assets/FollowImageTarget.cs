8ff9167355c6eee49ff1fa82ada36f47cdc13994
Paul Sorenson <paul.sorenson@zenuity.com>
2019-02-15 17:07:30 -0500

    scaled down occlusion model for vehicle and spheres around to test

---
+++ Assets/FollowImageTarget.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowImageTarget : MonoBehaviour
+{
+
+    public Transform imageTargetTransform;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (imageTargetTransform != null)
+        {
+            transform.position = imageTargetTransform.position;
+            transform.rotation = imageTargetTransform.rotation;
+        }
+    }
+}
